7ce3e3c75191f50d58abbdee1a9cb90a6860dc0f
Irvine Sunday <irochand@microsoft.com>
2020-08-18 12:12:37 +0300

    Change access modifier from public to internal; add parameter validation

--- PermissionsScraper/PermissionsScraper/Helpers/ProtectedApiCallHelper.cs
+++ PermissionsScraper/PermissionsScraper/Helpers/ProtectedApiCallHelper.cs
@@ -13,7 +13,7 @@ namespace PermissionsScraper.Helpers
     /// <summary>
     /// Helper class to call a protected API and returns the result of the call
     /// </summary>
-    public static class ProtectedApiCallHelper
+    internal static class ProtectedApiCallHelper
     {
         /// <summary>
         /// Calls a protected Web API
@@ -21,30 +21,35 @@ namespace PermissionsScraper.Helpers
         /// <param name="webApiUrl">Url of the Web API to call</param>
         /// <param name="accessToken">Access token used as a bearer security token to call the Web API</param>
         /// <returns>The response of the call to the Web API</returns>
-        public static async Task<string> CallWebApiAsync(string webApiUrl, string accessToken)
+        internal static async Task<string> CallWebApiAsync(string webApiUrl, string accessToken)
         {
-            if (!string.IsNullOrEmpty(accessToken))
+            if (string.IsNullOrEmpty(webApiUrl))
             {
-                var httpClient = new HttpClient();
-                var defaultRequestHeaders = httpClient.DefaultRequestHeaders;
-                if (defaultRequestHeaders.Accept == null || !defaultRequestHeaders.Accept.Any(m => m.MediaType == "application/json"))
-                {
-                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                }
-                defaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
-
-                HttpResponseMessage response = await httpClient.GetAsync(webApiUrl);
-                if (response.IsSuccessStatusCode)
-                {
-                    string json = await response.Content.ReadAsStringAsync();
-                    return json;
-                }
-                else
-                {
-                    throw new Exception(await response.Content.ReadAsStringAsync());
-                }
+                throw new ArgumentNullException(nameof(webApiUrl), "Parameter cannot be null or empty");
             }
-            return null;
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                throw new ArgumentNullException(nameof(accessToken), "Parameter cannot be null or empty");
+            }
+
+            var httpClient = new HttpClient();
+            var defaultRequestHeaders = httpClient.DefaultRequestHeaders;
+            if (defaultRequestHeaders.Accept == null || !defaultRequestHeaders.Accept.Any(m => m.MediaType == "application/json"))
+            {
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+            defaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+
+            HttpResponseMessage response = await httpClient.GetAsync(webApiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                return json;
+            }
+            else
+            {
+                throw new Exception(await response.Content.ReadAsStringAsync());
+            }
         }
     }
 }
