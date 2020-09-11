7eae60dffc26722f5894efa1526ca63d04a901e7
Irvine Sunday <40403681+irvinesunday@users.noreply.github.com>
2020-09-11 12:51:22 +0300

    Format GitHub App Key appropriately (#9)
    Co-authored-by: Irvine Sunday <irochand@microsoft.com>

--- PermissionsScraper/PermissionsScraper/Helpers/PermissionsFormatHelper.cs
+++ PermissionsScraper/PermissionsScraper/Helpers/PermissionsFormatHelper.cs
@@ -2,6 +2,7 @@
 //  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 // ------------------------------------------------------------------------------------------------------------------------------------------------------
 
+using System;
 using System.Text.RegularExpressions;
 using PermissionsScraper.Common;
 
@@ -17,6 +18,11 @@ namespace PermissionsScraper.Helpers
         /// <returns>The Service Principal JSON response data with the regex transformations applied.</returns>
         internal static string FormatServicePrincipalResponse(string spJsonResponse, ApplicationConfig config)
         {
+            if (string.IsNullOrEmpty(spJsonResponse))
+            {
+                throw new ArgumentNullException(nameof(spJsonResponse), "Parameter cannot be null or empty");
+            }
+
             string cleanedSpJson = spJsonResponse;
 
             foreach (var item in config.RegexPatterns)
@@ -27,6 +33,24 @@ namespace PermissionsScraper.Helpers
             }
 
             return cleanedSpJson;
+        }
+
+        /// <summary>
+        /// Formats the private key to add newline characters appropriately.
+        /// </summary>
+        /// <param name="key">The private key.</param>
+        /// <returns>The private key with newline characters appropriately added.</returns>
+        internal static string FormatPrivateKey(this string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException(nameof(key), "Parameter cannot be null or empty");
+            }
+
+            var sections = key.Split("-----BEGIN RSA PRIVATE KEY-----", StringSplitOptions.RemoveEmptyEntries);
+            sections = sections[0].Split("-----END RSA PRIVATE KEY-----", StringSplitOptions.RemoveEmptyEntries);
+
+            return "-----BEGIN RSA PRIVATE KEY-----\r\n" + sections[0] + "\r\n-----END RSA PRIVATE KEY-----";
         }
     }
 }
