0a8e46cb83c454fcdfd5fbc0939e9707ad1d34b8
Irvine Sunday <irochand@microsoft.com>
2021-07-16 02:06:14 +0300

    Add tests and test files

---
+++ PermissionsScraper/PermissionsScraper.Tests/Resources.cs
@@ -0,0 +1,33 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using PermissionsScraper.Common;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PermissionsScraper.Tests
+{
+    internal static class Resources
+    {
+        /// <summary>
+        /// Reads the contents of a provided file.
+        /// </summary>
+        /// <param name="filePathSource">The directory path name of the file.</param>
+        /// <returns>The contents of the file.</returns>
+        public static async Task<string> GetFileContents(string filePathSource)
+        {
+            UtilityFunctions.CheckArgumentNullOrEmpty(filePathSource, nameof(filePathSource));
+
+            try
+            {
+                using StreamReader streamReader = new(filePathSource);
+                return await streamReader.ReadToEndAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
