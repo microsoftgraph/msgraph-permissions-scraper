a9dc184e48328529ccac1d7094601685d348df01
Irvine Sunday <irochand@microsoft.com>
2023-02-14 16:40:41 +0300

    Add latest changes

---
+++ PermissionsScraper/PermissionsScraper.Tests/Services/PermissionsFilePathsServiceShould.cs
@@ -0,0 +1,31 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------using PermissionsScraper.Services;
+
+using PermissionsScraper.Services;
+using System;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace PermissionsScraper.Tests.Services
+{
+    public class PermissionsFilePathsServiceShould
+    {
+        private string _v1FileContents;
+        public PermissionsFilePathsServiceShould()
+        {
+            using var stream = new FileStream(Path.Combine(Environment.CurrentDirectory, "Files", "permissions-v1.0.json"), FileMode.Open);
+            using var reader = new StreamReader(stream, Encoding.UTF8);
+            _v1FileContents = reader.ReadToEnd();
+        }
+        
+        [Fact]
+        public void GetPermissionsPaths()
+        {
+            // Arrange
+            PermissionsFilePathsService service = new PermissionsFilePathsService();
+            service.GetPathsDictionaryFromPermissionsFileContents(_v1FileContents);
+        }
+    }
+}
--- PermissionsScraper/PermissionsScraper/Services/PermissionsFilePathsService.cs
+++ PermissionsScraper/PermissionsScraper/Services/PermissionsFilePathsService.cs
@@ -17,28 +17,42 @@ namespace PermissionsScraper.Services
     public class PermissionsFilePathsService
     {
         /// <summary>
-        /// Gets a dictionary of paths and http methods extracted from a permissions file located at the provided <paramref name="fileUrl"/>.
+        /// Gets the permission file from a url.
         /// </summary>
-        /// <param name="fileUrl">The target url which contains the permissions file.</param>
-        /// <returns>A dictionary of paths and http methods extracted from the permissions file retrieved at the provided <paramref name="fileUrl"/>.</returns>
-        /// <exception cref="InvalidOperationException"></exception>
-        public async Task<Dictionary<string, List<string>>> GetPathsDictionaryFromPermissionsFile(string fileUrl)
+        /// <param name="fileUrl">The url of the permissions file.</param>
+        /// <returns>The file contents of the permissions file.</returns>
+        /// <exception cref="ArgumentNullException">Exception thrown when the <paramref name="fileUrl"/> is empty or null.</exception>
+        public async Task<string> GetPermissionsFileFromUrl(string fileUrl)
         {
-            if (string.IsNullOrEmpty(fileUrl))
-            {
-                throw new ArgumentNullException(nameof(fileUrl));
-            }
-            
-            // Extract permissions from devx-content repo
+            UtilityFunctions.CheckArgumentNullOrEmpty(fileUrl, nameof(fileUrl));
+           
             var client = HttpClientSingleton.Instance;
             using var response = await client.HttpClient.GetAsync(fileUrl);
-            var content = await response.Content.ReadAsStringAsync();      
-            if (string.IsNullOrEmpty(content))
+            if (!response.IsSuccessStatusCode)
             {
-                throw new ArgumentNullException(nameof(fileUrl), "No permissions data found for the given url " + fileUrl);
+                throw new Exception("Failed to get the file at the provided url: " + fileUrl);
             }
 
-            JObject permissionsObject = JObject.Parse(content);
+            var content = await response.Content.ReadAsStringAsync();
+            if (string.IsNullOrEmpty(content))
+            {
+                throw new ArgumentNullException(nameof(fileUrl), "No permissions data found for the provided url " + fileUrl);
+            }
+
+            return content.ToString();
+        }
+        
+        /// <summary>
+        /// Gets a dictionary of paths and http methods extracted from a given permissions file.
+        /// </summary>
+        /// <param name="fileContent">The contents of the permissions file.</param>
+        /// <returns>A dictionary of paths and http methods extracted from the permissions file.</returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public Dictionary<string, List<string>> GetPathsDictionaryFromPermissionsFileContents(string fileContent)
+        {
+            UtilityFunctions.CheckArgumentNullOrEmpty(fileContent, nameof(fileContent));
+            
+            JObject permissionsObject = JObject.Parse(fileContent);
             if (permissionsObject.Count < 1)
             {
                 throw new InvalidOperationException("No permissions data found.");
@@ -62,6 +76,19 @@ namespace PermissionsScraper.Services
             }
 
             return permPathsDictionary;
+        }
+
+        /// <summary>
+        /// Gets a dictionary of paths and http methods extracted from a file at the given url. 
+        /// </summary>
+        /// <param name="fileUrl">The url of the permissions file.</param>
+        /// <returns>A dictionary of paths and http methods extracted from the permissions file located at the provided <paramref name="fileUrl"/>.</returns>
+        public async Task<Dictionary<string, List<string>>> GetPathsDictionaryFromPermissionsFileUrl(string fileUrl)
+        {
+            UtilityFunctions.CheckArgumentNull(fileUrl, nameof(fileUrl));
+
+            var permissionsFile = await GetPermissionsFileFromUrl(fileUrl);
+            return GetPathsDictionaryFromPermissionsFileContents(permissionsFile);
         }
     }
 }
