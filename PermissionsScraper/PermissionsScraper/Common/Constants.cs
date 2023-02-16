1c568f2822d74b6628c7da20efb865e246eeda5e
Maggie Kimani <maggiekim42@gmail.com>
2023-02-16 15:06:57 +0300

    Add a function trigger for uploading graph path files post analysis

---
+++ PermissionsScraper/PermissionsScraper/Common/Constants.cs
@@ -0,0 +1,21 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace PermissionsScraper.Common
+{
+    public static class Constants
+    {
+        public static string PermissionsDescriptions = "PermissionsDescriptions";
+        public static string OpenApiPathsV1 = "OpenApiPathsV1";
+        public static string OpenApiPathsBeta = "OpenApiPathsBeta";
+        public static string PermissionsPathsV1 = "PermissionsPathsV1";
+        public static string PermissionsPathsBeta = "PermissionsPathsBeta";
+        public static string PathsNotInPermissionsFileV1 = "PathsNotInPermissionsFileV1";
+        public static string PathsNotInPermissionsFileBeta = "PathsNotInPermissionsFileBeta";
+        public static string PathsNotInOpenApiDocumentV1 = "PathsNotInOpenApiDocumentV1";
+        public static string PathsNotInOpenApiDocumentBeta = "PathsNotInOpenApiDocumentBeta";
+        public static string V1 = "v1.0";
+        public static string Beta = "beta";
+    }
+}
